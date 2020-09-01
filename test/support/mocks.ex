368cb1acf7e4dab469c2394455b17e9805f82314
David Kovsky <davidkovsky@gmail.com>
2020-08-31 21:02:44 -0600

    adds optional logging of mocked and unmocked function calls

--- test/support/mocks.ex
+++ test/support/mocks.ex
@@ -9,3 +9,9 @@ Mox.defmock(SciCalcMockWithoutOptional,
 Mox.defmock(MyMockWithoutModuledoc, for: Calculator)
 Mox.defmock(MyMockWithFalseModuledoc, for: Calculator, moduledoc: false)
 Mox.defmock(MyMockWithStringModuledoc, for: Calculator, moduledoc: "hello world")
+
+Mox.defmock(MyMockWithMockedCallLogging, for: Calculator, log_mocked_calls: true)
+Mox.defmock(MyMockWithUnmockedCallLogging, for: Calculator, log_unmocked_calls: true)
+
+Mox.defmock(MyMockWithoutMockedCallLogging, for: Calculator, log_mocked_calls: false)
+Mox.defmock(MyMockWithoutUnmockedCallLogging, for: Calculator, log_unmocked_calls: false)
